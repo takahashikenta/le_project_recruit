cc7342d9dd8bf12685404a764f46c402eae58021
masasuzuki <match.suzuki@gmail.com>
2017-07-31 10:25:37 +0900

    20170731：本番反映前

--- mail_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(mb_send_mail("match.suzuki@gmail.com", "てすと", "これはテストメールです\n登録ありがとうございます\nテストメール", "From: match.suzuki@gmail.com", '-f match.suzuki@gmail.com')){
-	echo "メールが送信されました";
-}else{
-	echo "メールの送信に失敗しました";
-}
-
-?>
